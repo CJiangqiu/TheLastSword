9c7e8a3882e650e57874c21a3ad6b98f809f2edf
CJiangqiu <17787153839@qq.om>
2024-08-28 21:46:25 +0800

    1.0.5

---
+++ src/main/java/net/thelastsword/network/ChangeTheLastSwordModeMessage.java
@@ -0,0 +1,70 @@
+package net.thelastsword.network;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.network.NetworkEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.thelastsword.capability.SwordCapability;
+import net.thelastsword.item.TheLastSword;
+
+import java.util.function.Supplier;
+
+public class ChangeTheLastSwordModeMessage {
+    public ChangeTheLastSwordModeMessage() {}
+
+    public ChangeTheLastSwordModeMessage(FriendlyByteBuf buffer) {}
+
+    public static void buffer(ChangeTheLastSwordModeMessage message, FriendlyByteBuf buffer) {}
+
+    public static void handler(ChangeTheLastSwordModeMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+        NetworkEvent.Context context = contextSupplier.get();
+        context.enqueueWork(() -> {
+            Player player = context.getSender();
+            if (player != null) {
+                ItemStack itemstack = player.getMainHandItem();
+                if (itemstack.getItem() instanceof TheLastSword) {
+                    itemstack.getCapability(SwordCapability.MODE_CAPABILITY).ifPresent(modeCapability -> {
+                        int mode = modeCapability.getMode();
+                        mode = (mode + 1) % 3; // 切换模式
+                        modeCapability.setMode(mode);
+
+                        // 更新模式信息
+                        itemstack.getOrCreateTag().putInt("Mode", mode);
+
+
+                        // 更新tooltip
+
+                        player.inventoryMenu.broadcastChanges(); // 强制刷新物品状态
+
+                        // 强制刷新物品栏
+                        //player.getInventory().setChanged();
+
+                        // 发送消息给玩家
+                        String modeText = getModeText(mode);
+                        player.displayClientMessage(Component.translatable("item_tooltip.the_last_sword.mode").append(Component.translatable(modeText)), true);
+
+                        // 播放声音
+                        if (!player.level().isClientSide()) {
+                            player.level().playSound(null, player.blockPosition(), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.ender_dragon.flap")), SoundSource.HOSTILE, 1, 1);
+                        } else {
+                            player.level().playLocalSound(player.getX(), player.getY(), player.getZ(), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.ender_dragon.flap")), SoundSource.HOSTILE, 1, 1, false);
+                        }
+                    });
+                }
+            }
+        });
+        context.setPacketHandled(true);
+    }
+
+    private static String getModeText(int mode) {
+        return switch (mode) {
+            case 1 -> "item_tooltip.the_last_sword.powerful_mining_mode";
+            case 2 -> "item_tooltip.the_last_sword.powerful_range_attack_mode";
+            default -> "item_tooltip.the_last_sword.normal_mode";
+        };
+    }
+}
